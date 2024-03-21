f56162584e2e47a427f65f8769e68d8ab7722733
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-03-21 16:02:57 +0800

    ASTON-8 [refactor]: added logic with LocalStorage

--- src/page/sign-in-page/sign-in-page.tsx
+++ src/page/sign-in-page/sign-in-page.tsx
@@ -1,25 +1,49 @@
+import { ChangeEvent, MouseEvent, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Button } from '@/components/button'
+import { CardBg } from '@/components/cardBg'
 import { TextField } from '@/components/textField'
 
 import s from './sign-in-page.module.scss'
 
-import { CardBg } from '../../components/cardBg'
-
 export const SignInPage = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value)
+  }
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value)
+  }
+  const getItem = () => localStorage.getItem('user')
+  const signInHandler = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    console.log(getItem())
+  }
+
   return (
     <CardBg className={s.container}>
       <h2 className={s.title}>Sign in</h2>
       <form className={s.form}>
-        <TextField label={'Email'} name={'email'} placeholder={'email'} />
+        <TextField
+          label={'Email'}
+          name={'email'}
+          onChange={onChangeEmail}
+          placeholder={'email'}
+          value={email}
+        />
         <TextField
           label={'Password'}
           name={'password'}
+          onChange={onChangePassword}
           placeholder={'password'}
           type={'password'}
+          value={password}
         />
-        <Button>Sign In</Button>
+        <Button onClick={e => signInHandler(e)}>Sign In</Button>
       </form>
       <div className={s.footer}>
         <div className={s.text}>{"Don't have an account?"}</div>
--- src/page/sign-up-page/sign-up-page.tsx
+++ src/page/sign-up-page/sign-up-page.tsx
@@ -1,31 +1,60 @@
+import { ChangeEvent, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Button } from '@/components/button'
+import { CardBg } from '@/components/cardBg'
 import { TextField } from '@/components/textField'
 
 import s from './sign-up-page.module.scss'
 
-import { CardBg } from '../../components/cardBg'
-
 export const SignUpPage = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [secondPassword, setSecondPassword] = useState<string>('')
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value)
+  }
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value)
+  }
+  const onSecondChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setSecondPassword(e.currentTarget.value)
+  }
+  const signUpHandler = () => {
+    if (password !== secondPassword) {
+      return // TODO Пока заглушка
+    }
+    localStorage.setItem('user', JSON.stringify({ email, password }))
+  }
+
   return (
     <CardBg className={s.container}>
       <h2 className={s.title}>Sign in</h2>
       <form className={s.form}>
-        <TextField label={'Email'} name={'email'} placeholder={'email'} />
+        <TextField
+          label={'Email'}
+          name={'email'}
+          onChange={onChangeEmail}
+          placeholder={'email'}
+          value={email}
+        />
         <TextField
           label={'Password'}
           name={'password'}
+          onChange={onChangePassword}
           placeholder={'password'}
           type={'password'}
-        />{' '}
+          value={password}
+        />
         <TextField
           label={'Confirm Password'}
           name={'Confirm Password'}
+          onChange={onSecondChangePassword}
           placeholder={'Confirm Password'}
           type={'password'}
+          value={secondPassword}
         />
-        <Button>Sign Up</Button>
+        <Button onClick={signUpHandler}>Sign Up</Button>
       </form>
       <div className={s.footer}>
         <div className={s.text}>Already have an account?</div>
