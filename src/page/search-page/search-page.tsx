28e55fc3fefa0f81e49945f139891dedd6bf9a57
Andrey Morogin <andreandreandre10071998@yandex.ru>
2024-03-26 16:31:17 +0300

    ASTON-18 [refactor]: delete hook useResize and added adaptive css

--- src/page/search-page/search-page.tsx
+++ src/page/search-page/search-page.tsx
@@ -8,11 +8,10 @@ import { Filters } from '@/components/filters'
 import { FiltersContainer } from '@/components/filtersContainer'
 import { Pagination } from '@/components/pagination'
 import { Search } from '@/components/search'
-import { useResize } from '@/hooks/use-resize'
 import { useResourceFiltering } from '@/hooks/use-resource-filtering'
 import { Character, CharactersApi } from '@/service/ResoursesService/CharactersApi'
 import { Info } from '@/service/ServicePrototype'
-import { checkIsDesktop, getPageSize } from '@/utils'
+import { getPageSize } from '@/utils'
 
 import s from './search-page.module.scss'
 
@@ -34,7 +33,6 @@ export const SearchPage: FC = () => {
 
   const { handleButtonClear, handleChange, handleSearch, search, urlParams } =
     useResourceFiltering()
-  const width = useResize()
 
   const handleFilterPopup = () => {
     setIsPopup(!isPopup)
@@ -83,26 +81,24 @@ export const SearchPage: FC = () => {
   }, [urlParams])
 
   const pageSize = getPageSize(info.count, info.pages)
-  const isDesctop = checkIsDesktop(width)
 
   return (
     <>
       <section className={`${s.page__section} ${s.page__section_search}`}>
-        <Search clearValue={handleButtonClear} onChange={handleChange} value={search.name} />
+        <Search
+          className={s.page__search}
+          clearValue={handleButtonClear}
+          onChange={handleChange}
+          value={search.name}
+        />
       </section>
       <div className={s.page__container}>
-        {isDesctop ? (
-          <Filters cbClear={handleButtonClear} cbRadio={handleSearch} state={search} />
-        ) : (
-          <Button
-            // eslint-disable-next-line react/no-children-prop
-            children={'Filters'}
-            className={s.page__button}
-            disabled={false}
-            onClick={handleFilterPopup}
-            variant={'secondary'}
-          />
-        )}
+        <Filters
+          cbClear={handleButtonClear}
+          cbRadio={handleSearch}
+          className={s.page__filters}
+          state={search}
+        />
         <section className={s.page__section}>
           <CharacterList chars={chars} className={s.page__list} />
           {info.pages > 1 && (
@@ -116,6 +112,14 @@ export const SearchPage: FC = () => {
           )}
         </section>
       </div>
+      <Button
+        // eslint-disable-next-line react/no-children-prop
+        children={'Filters'}
+        className={s.page__button}
+        disabled={false}
+        onClick={handleFilterPopup}
+        variant={'secondary'}
+      />
       <FiltersContainer
         cbClear={handleButtonClear}
         cbPopup={handleFilterPopup}
