22d56b3e35e8602c163795d15241ab7af546d0a1
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-03-29 20:08:05 +0800

    ASTON-27 [feat]: add jest library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,16 @@
 module.exports = {
-  extends: ["@it-incubator/eslint-config", "plugin:storybook/recommended", "prettier"],
+  extends: ['@it-incubator/eslint-config', 'plugin:storybook/recommended', 'prettier'],
   overrides: [
     {
-      files: ['**/*.stories.tsx'],
+      files: ['**/*.stories.tsx',  '**/*.test.tsx'],
       rules: {
         'no-console': 'off',
         'react-hooks/rules-of-hooks': 'off',
       },
+      'plugins': ['jest'],
+      'env': {
+        'jest/globals': true
+      }
     },
   ],
-}
+};
