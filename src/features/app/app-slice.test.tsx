1869ea4f04b97ace10c3d626f5787a5309272200
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-03-31 19:44:53 +0800

    ASTON-27 [fix]: wrapped describe tests

--- src/features/app/app-slice.test.tsx
+++ src/features/app/app-slice.test.tsx
@@ -11,26 +11,28 @@ beforeEach(() => {
     isLoading: false,
   }
 })
-test('correct error should be set', () => {
-  const error = 'This is big error'
-  const endState = appSlice.reducer(startState, setError({ error }))
+describe('Work with app slice ', () => {
+  test('correct error should be set', () => {
+    const error = 'This is big error'
+    const endState = appSlice.reducer(startState, setError({ error }))
 
-  expect(endState.error).toBe(error)
-})
-test('correct error should be null', () => {
-  const endState = appSlice.reducer(startState, setError({ error: null }))
+    expect(endState.error).toBe(error)
+  })
+  test('correct error should be null', () => {
+    const endState = appSlice.reducer(startState, setError({ error: null }))
 
-  expect(endState.error).toBe(null)
-})
-test('should be set loading', () => {
-  const status = true
-  const endState = appSlice.reducer(startState, setLoading({ status }))
+    expect(endState.error).toBe(null)
+  })
+  test('should be set loading', () => {
+    const status = true
+    const endState = appSlice.reducer(startState, setLoading({ status }))
 
-  expect(endState.isLoading).toBe(status)
-})
-test('state should be initialized', () => {
-  const status = true
-  const endState = appSlice.reducer(startState, setInitialized({ status }))
+    expect(endState.isLoading).toBe(status)
+  })
+  test('state should be initialized', () => {
+    const status = true
+    const endState = appSlice.reducer(startState, setInitialized({ status }))
 
-  expect(endState.initialized).toBe(status)
+    expect(endState.initialized).toBe(status)
+  })
 })
--- src/features/auth/auth-slice.test.tsx
+++ src/features/auth/auth-slice.test.tsx
@@ -11,28 +11,29 @@ beforeEach(() => {
     isAuth: false,
   }
 })
+describe('Work with auth slice ', () => {
+  test('should be login', () => {
+    const email = '1@1.ru'
+    const endState = authSlice.reducer(startState, login({ email }))
 
-test('should be login', () => {
-  const email = '1@1.ru'
-  const endState = authSlice.reducer(startState, login({ email }))
+    expect(endState.email).toBe(email)
+    expect(endState.isAuth).toBe(true)
+  })
+  test('should be logOut', () => {
+    const endState = authSlice.reducer(startState, logout())
 
-  expect(endState.email).toBe(email)
-  expect(endState.isAuth).toBe(true)
-})
-test('should be logOut', () => {
-  const endState = authSlice.reducer(startState, logout())
+    expect(endState.email).toBe(null)
+    expect(endState.isAuth).toBe(false)
+  })
+  test('correct error should be set', () => {
+    const error = 'This is big error'
+    const endState = authSlice.reducer(startState, setError({ error }))
 
-  expect(endState.email).toBe(null)
-  expect(endState.isAuth).toBe(false)
-})
-test('correct error should be set', () => {
-  const error = 'This is big error'
-  const endState = authSlice.reducer(startState, setError({ error }))
+    expect(endState.error).toBe(error)
+  })
+  test('correct error should be null', () => {
+    const endState = authSlice.reducer(startState, setError({ error: null }))
 
-  expect(endState.error).toBe(error)
-})
-test('correct error should be null', () => {
-  const endState = authSlice.reducer(startState, setError({ error: null }))
-
-  expect(endState.error).toBe(null)
+    expect(endState.error).toBe(null)
+  })
 })
