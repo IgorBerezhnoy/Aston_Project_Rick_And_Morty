5b7cbddaf91e3ffccf05e0d77dfc8758a3e06b9b
MolchanovEG <146551641+MolchanovEG@users.noreply.github.com>
2024-04-01 14:21:40 +0300

    ASTON-20 [fix]: delete log

--- src/components/history/historyContainer.tsx
+++ src/components/history/historyContainer.tsx
@@ -7,8 +7,6 @@ import { History } from './history'
 const HistoryContainer = () => {
   const [history, setHistory] = useState(useAppSelector(state => state.auth.user))
 
-  console.log(history)
-
   if (!history?.stories) {
     return <History history={null} />
   }
