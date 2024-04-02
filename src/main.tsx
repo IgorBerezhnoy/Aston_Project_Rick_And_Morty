f3f54753e6d1bd2c980884d2476542d5408c76e0
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-04-02 14:46:01 +0800

    ASTON-31 [fix]: correcting work with store

--- src/main.tsx
+++ src/main.tsx
@@ -9,14 +9,10 @@ import './styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 
-import { storeRTKQuery } from './app/store'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={storeRTKQuery}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>
 )
