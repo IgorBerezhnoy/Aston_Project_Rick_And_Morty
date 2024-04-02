17f47038043606a04feb4c0ed6b2f08b135da8ab
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-04-02 15:59:48 +0800

    ASTON-20 [refactor]: added cardPage into history page

--- src/page/history-page/history-page.tsx
+++ src/page/history-page/history-page.tsx
@@ -1,37 +1,37 @@
 import { Link } from 'react-router-dom'
 
+import { CardPage } from '@/components/cardPage'
 import { User } from '@/features/auth/authSlice'
 
 import s from './history-page.module.scss'
 
 import { randomEmoji } from './lib/randomEmoji'
+
 type Props = {
   history: User | null
 }
 
 export const History = ({ history }: Props) => {
-  if (!history || !history.stories.length) {
-    return (
-      <div className={s.titleContainer}>
-        <h1 className={s.titleName}>History is empty</h1>
-      </div>
-    )
-  }
-
   return (
     <div className={s.pageContainer}>
       <div className={s.titleContainer}>
         <h1 className={s.titleName}>Search history</h1>
       </div>
       <div className={s.historyContainer}>
-        {history.stories.map((item, index) => (
-          <div className={s.historyItem} key={index}>
-            <Link className={s.historyItemLink} to={`/search/?page=1&name=${item.name}`}>
-              {randomEmoji()}
-              {item.name}
-            </Link>
-          </div>
-        ))}
+        {history?.stories.length ? (
+          <>
+            {history.stories.map((item, index) => (
+              <div className={s.historyItem} key={index}>
+                <Link className={s.historyItemLink} to={`/search/?page=1&name=${item.name}`}>
+                  {randomEmoji()}
+                  {item.name}
+                </Link>
+              </div>
+            ))}
+          </>
+        ) : (
+          <CardPage title={"Sorry you don't have any history"} />
+        )}
       </div>
     </div>
   )
