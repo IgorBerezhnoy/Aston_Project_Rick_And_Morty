1a49da5f5060b7c4dc2aea9759bb35824363e69a
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-04-03 15:00:14 +0800

    ASTON-29 [fix]: correcting meta tags

--- src/page/character-page/lib/characterShare.tsx
+++ src/page/character-page/lib/characterShare.tsx
@@ -14,24 +14,24 @@ import {
 import s from '../character-page.module.scss'
 
 export const CharacterShare = ({ name }: { name: string }) => {
-  const currentPageUrl = window.location.href
-  const title = `Hi! I'm looking for Rick and Morty character ${name}`
+  const url = 'https://aston-project-rick-and-morty.vercel.app/'
+  const title = `Hi! I'm looking for Rick and Morty character ${name} on Aston Project. Check it out! ${window.location.href}`
 
   return (
     <div className={s.characterShare}>
-      <RedditShareButton className={s.characterShareButton} title={title} url={currentPageUrl}>
+      <RedditShareButton className={s.characterShareButton} title={title} url={url}>
         <RedditIcon round />
       </RedditShareButton>
-      <TelegramShareButton className={s.characterShareButton} title={title} url={currentPageUrl}>
+      <TelegramShareButton className={s.characterShareButton} title={title} url={url}>
         <TelegramIcon round />
       </TelegramShareButton>
-      <VKShareButton className={s.characterShareButton} title={title} url={currentPageUrl}>
+      <VKShareButton className={s.characterShareButton} title={title} url={url}>
         <VKIcon round />
       </VKShareButton>
-      <ViberShareButton className={s.characterShareButton} title={title} url={currentPageUrl}>
+      <ViberShareButton className={s.characterShareButton} title={title} url={url}>
         <ViberIcon round />
       </ViberShareButton>
-      <WhatsappShareButton className={s.characterShareButton} title={title} url={currentPageUrl}>
+      <WhatsappShareButton className={s.characterShareButton} title={title} url={url}>
         <WhatsappIcon round />
       </WhatsappShareButton>
     </div>
