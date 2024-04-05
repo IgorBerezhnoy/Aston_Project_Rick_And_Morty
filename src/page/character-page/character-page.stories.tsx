8f8d40d8e23c86cc2d0a118b3dcc5fcd917c6ffb
Andrey Morogin <andreandreandre10071998@yandex.ru>
2024-04-05 14:37:45 +0300

    ASTON-32 [fix]: fix story character page

--- src/page/character-page/character-page.stories.tsx
+++ src/page/character-page/character-page.stories.tsx
@@ -1,9 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { useState } from 'react'
+import { Provider } from 'react-redux'
+
+import { store } from '@/app/store/store'
 import { CharacterPage } from '@/page/character-page/character-page'
 
 const meta: Meta<typeof CharacterPage> = {
   component: CharacterPage,
+  decorators: [
+    Story => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
   tags: ['autodocs'],
   title: 'Components/UI/CharacterPage',
 } satisfies Meta<typeof CharacterPage>
@@ -35,6 +46,16 @@ const rickData = {
   url: 'https://rickandmortyapi.com/api/character/1',
 }
 
+const CharacterPageWithButton = () => {
+  const [isFavorite, setIsFavorite] = useState(false)
+
+  function handleButtonClick() {
+    setIsFavorite(!isFavorite)
+  }
+
+  return <CharacterPage cbClick={handleButtonClick} char={rickData} isFavorite={isFavorite} />
+}
+
 export const Default: Story = {
-  render: () => <CharacterPage char={rickData} />,
+  render: () => <CharacterPageWithButton />,
 }
